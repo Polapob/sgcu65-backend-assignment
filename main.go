b423374a7e25be667168a3bc36e85440243ada41
Polapob <71330294+Polapob@users.noreply.github.com>
2022-07-02 20:43:33 +0700

    refactor main.go

--- main.go
+++ main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
-	"log"
-	"sgcu65/controllers"
 	"sgcu65/models"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+	"sgcu65/router"
 )
 
 func main() {
-	r := gin.Default()
-	err := godotenv.Load()
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	db := models.ConnectDatabase()
 
-	db := controllers.NewDBController(models.ConnectDatabase()) // new
+	router.SetUpRouter(db)
 
-	r.POST("/task/", db.AddTask)
-	r.DELETE("/task/:id", db.DeleteTask)
-	r.GET("/task/:id", db.GetTask)
-	r.POST("/user/", db.AddUser)
-	r.GET("/user/:id", db.GetUser)
-	r.DELETE("/user/:id", db.DeleteUser)
-	r.POST("/team", db.AddTeam)
-	r.DELETE("/team/:id", db.DeleteTeam)
-	r.GET("/team/:id", db.GetTeam)
-	r.Run()
 }
